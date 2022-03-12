9c466b1919940cc0373e23b22089e151e85e4c6c
gsomer <gasomer75@hotmail.com>
2022-03-11 22:51:56 -0800

    add express.js and mysql2 to connect to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to the database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password,
+    password: 'Onebigword',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for bad requests (Not Found)
 app.use((req, res) => {
